814eb37f8abb4b5d2ebaa53af237fbb552e041fd
Dev Koshti <dev.k@digipay.guru>
2025-02-20 18:45:41 +0530

    [add] new data

--- src/main/java/com/example/User/UserApplication.java
+++ src/main/java/com/example/User/UserApplication.java
@@ -1,13 +1,32 @@
 package com.example.User;
 
+import com.example.User.statemachine.StateMachineAccessor;
+import com.example.User.statemachine.states.AppEvents;
+import com.example.User.statemachine.states.AppState;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class UserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 
+    @Component
+    @RequiredArgsConstructor
+    class Runner implements ApplicationRunner {
+        private final StateMachineAccessor stateMachineAccessor;
+
+        @Override
+        public void run(ApplicationArguments args) {
+            StateMachine<AppState, AppEvents> stateMachine = stateMachineAccessor.getStateMachine();
+            stateMachine.start();
+        }
+    }
 }
